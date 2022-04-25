ce4b6c48f2ae53d54bdec8a8239a9b6adf4556c9
Sean Pietersen <seanpietersen7@gmail.com>
2022-04-25 15:13:11 +0200

    Feat: Added Test

---
+++ GiveMeADiamond/GiveMeADiamond.Test/CalculateADimondShapeStringTest.cs
@@ -0,0 +1,104 @@
+using System.Text;
+using Xunit;
+
+namespace GiveMeADiamond.Test
+{
+    public class CalculateADimondShapeStringTest
+    {
+        [Fact]
+        public void printDiamondShapeStringTest1_ShouldReturnNull_NegativeInt()
+        {
+            //Arrange
+            ICalculateADiamondShapeString calculateADiamondShapeString = new CalculateADimondShapeString();
+            int n = -2;
+
+            //Act
+            var actual = calculateADiamondShapeString.printDiamondShapeString(n);
+
+            //Assert
+            Assert.Null(actual);
+        }
+
+        [Fact]
+        public void printDiamondShapeStringTest2_ShouldReturnNull_IntIsNull()
+        {
+            //Arrange
+            ICalculateADiamondShapeString calculateADiamondShapeString = new CalculateADimondShapeString();
+            int n = 0;
+
+            //Act
+            var actual = calculateADiamondShapeString.printDiamondShapeString(n);
+
+            //Assert
+            Assert.Null(actual);
+        }
+
+        [Fact]
+        public void printDiamondShapeStringTest3_ShouldReturnNull_IntIsEven()
+        {
+            //Arrange
+            ICalculateADiamondShapeString calculateADiamondShapeString = new CalculateADimondShapeString();
+            int n = 2;
+
+            //Act
+            var actual = calculateADiamondShapeString.printDiamondShapeString(n);
+
+            //Assert
+            Assert.Null(actual);
+        }
+
+        [Fact]
+        public void printDiamondShapeStringTest4_Successful()
+        {
+            //Arrange
+            ICalculateADiamondShapeString calculateADiamondShapeString = new CalculateADimondShapeString();
+            int n = 1;
+            var expected = new StringBuilder();
+            expected.Append("*\n");
+
+            //Act
+            var actual = calculateADiamondShapeString.printDiamondShapeString(n);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void printDiamondShapeStringTest5_Successful()
+        {
+            //Arrange
+            ICalculateADiamondShapeString calculateADiamondShapeString = new CalculateADimondShapeString();
+            int n = 3;
+            var expected = new StringBuilder();
+            expected.Append(" *\n");
+            expected.Append("***\n");
+            expected.Append(" *\n");
+
+            //Act
+            var actual = calculateADiamondShapeString.printDiamondShapeString(n);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void printDiamondShapeStringTest6_Successful()
+        {
+            //Arrange
+            ICalculateADiamondShapeString calculateADiamondShapeString = new CalculateADimondShapeString();
+            int n = 5;
+            var expected = new StringBuilder();
+            expected.Append("  *\n");
+            expected.Append(" ***\n");
+            expected.Append("*****\n");
+            expected.Append(" ***\n");
+            expected.Append("  *\n");
+
+            //Act
+            var actual = calculateADiamondShapeString.printDiamondShapeString(n);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
